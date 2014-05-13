c8472fdc86a931be8e0814b58da0bca8453406ab
leph00 <leph_00@yahoo.com>
2014-05-13 17:35:53 -0400

    initial commit for finalQuiz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Finalquiz::Application.config.secret_key_base = '631c3fafc07d1ed5eb28cb473cc888008ce2589d86c68264b22fcc5c92597e4f92e7cb24ece3423c2b47bfeecac2beb695bdf33911d0ed63b2ef168ebb10fa2a'
